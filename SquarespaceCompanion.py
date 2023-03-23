98314e615e5f48348e336564a549a5fcf2f657a7
Jonathan <jhamilton2394@gmail.com>
2023-03-22 21:56:03 -0700

    periodic commit

--- SquarespaceCompanion.py
+++ SquarespaceCompanion.py
@@ -8,64 +8,10 @@ import customtkinter as ctk
 import requests
 import time
 import menu as m
+import pagesFunctions as pf
 
 
 
-### CURRENTLY WORKING ON RETREIVING PAGE ID FOR USE IN PRODUCT CREATION
-### gets a list of pages and their id's, as well as some other random info that is included.
-#   returns name and id of specified page number only. This function is only intended to be
-#   called by the function 'pagesList'.
-def getPagesList(x):
-    getStorePagesURL = 'https://api.squarespace.com/1.0/commerce/store_pages'
-    getStorePagesHeaders = {'Authorization': 'Bearer ' + k.apiKey,
-                            'User-Agent': 'APIAPP1.0'}
-    r = requests.get(getStorePagesURL, headers=getStorePagesHeaders)
-    # data is the dictionary returned by the request.
-    data = r.json()
-    # storePages is the 2nd key in dictionary, its value is a list of dictionaries.
-    storePages = data['storePages']
-    # pageOne accesses the first element of the list, which is a dictionary of the
-    # first page's info. If there are multiple store pages then this is a good
-    # place to start a for loop to iterate through them.
-    pageOne = storePages[x]
-    # pageOne has all the info we need for now. pageID and pageName are accessing
-    # the specific keys and values needed for our current use.
-    pageID = pageOne['id']
-    pageName = pageOne['title']
-    pageInfo = {pageName: pageID}
-    return pageInfo
-
-
-### getNumOfPages returns the number of pages. It can be called by itself if the user
-#   wants the number of pages, and is also called by pagesList to be used for the range
-#   of pages to be iterated over.
-def getNumOfPages():
-    getStorePagesURL = 'https://api.squarespace.com/1.0/commerce/store_pages'
-    getStorePagesHeaders = {'Authorization': 'Bearer ' + k.apiKey,
-                            'User-Agent': 'APIAPP1.0'}
-    r = requests.get(getStorePagesURL, headers=getStorePagesHeaders)
-    # data is the dictionary returned by the request.
-    data = r.json()
-    # storePages is the 2nd key in dictionary, its value is a list of dictionaries.
-    storePages = data['storePages']
-    numOfPages = len(storePages)
-    return numOfPages
-
-
-### pagesList shows a list of all pages and their id's. Calls getNumOfPages and
-#   getPagesList in order to iterate over all pages.
-def pagesList():
-    PagesNo = getNumOfPages()
-    # erases any previously saved data from 'pages.txt' file
-    open('pages.txt', 'w').close()
-    # iterates over pages and writes them to file
-    for i in range(0, PagesNo):
-        page = getPagesList(i)
-        file1 = open('pages.txt', 'w')
-        
-        file1.write(str(page))
-
-pagesList()
 
 ###CURRENTLY WORKING ON UI USING CUSTOMTKINTER
 #window
@@ -138,10 +84,3 @@ pagesList()
 
 
 
-{'pagination': {'nextPageUrl': None,
-                'nextPageCursor': None,
-                'hasNextPage': False},
-'storePages': [{'id': '641a906c3f58246e8a8fe285',
-                'title': 'All products',
-                'isEnabled': True,
-                'urlSlug': 'all-products'}]}
