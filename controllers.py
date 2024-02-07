1f0f60dd61e03974844ad208c2c1b72a9dd1f163
Jonathan <jhamilton2394@gmail.com>
2024-02-06 18:25:53 -0800

    Auth build

--- controllers.py
+++ controllers.py
@@ -3,19 +3,13 @@ from model_auth_user import *
 class AuthController:
     def __init__(self):
         self.active_user = None
-            
-    def auth_controller(self, username, password):
+                    
+    def login_user(self, username, password):
         '''
         Called by LoginView's login method upon login attempt. Creates a temp_user then calls the User models get_user method.
         If the get_user method returns a user instance then the temp_user and the matched_users passwords are compared. If
         successful the user is assigned as active and the instance is returned to the login view.
         '''
-        user = User(username, password)
-        if user.login_user():
-            self.active_user = user.login_user()
-            return True
-        
-    def login_user(self, username, password):
         temp_user = User(username, password)
         matched_user = temp_user.get_user()
         if matched_user:
