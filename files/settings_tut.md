67b608d013ae64eabc1529f1339358ec697e8afe
biscuitbuns23 <jhamilton2394@gmail.com>
2024-03-31 20:13:19 -0700

    settings tutorial display update

---
+++ files/settings_tut.md
@@ -0,0 +1,20 @@
+In order for Squarespace Companion to interface with your inventory spreadsheet 
+it needs to know the column headers that are used. For example; if your items are 
+listed in rows, you probably have data such as "name", "description", and "price", 
+set as the column headers. 
+
+The column headers that are required to be configured are: 
+name 
+SKU 
+item description 
+price 
+quantity
+deleted
+
+If your inventory spreadsheet does not have columns with this info then you must 
+create them. This is the minimum info needed to create new products. 
+
+How to set the configuration: 
+Input the names of your column headers in the fields above and click save. You
+must type them EXACTLY as they appear on the spreadsheet. This only needs to be
+done once unless you change the headers on your excel file.
--- views.py
+++ views.py
@@ -204,7 +204,7 @@ class InfoView(BaseView):
     def destroy(self):
         self.announcement_box.destroy()
         
-class SettingsView():
+class SettingsView(BaseView):
     """Opens the settings menu when toggled."""
 
     def __init__(self, parent="self", *args, **kwargs):
@@ -287,27 +287,7 @@ class SettingsView():
 
         # Instruction box
         self.announcement_box = tk.Text(self.settings_frame, wrap="word", width=75)
-        self.announcement_box.insert("1.0",
-                                     """In order for Squarespace Companion to interface with your inventory spreadsheet 
-it needs to know the column headers that are used. For example; if your items are 
-listed in rows, you probably have data such as "name", "description", and "price", 
-set as the column headers. 
-
-The column headers that are required to be configured are: 
-name 
-SKU 
-item description 
-price 
-quantity
-deleted
-
-If your inventory spreadsheet does not have columns with this info then you must 
-create them. This is the minimum info needed to create new products. 
-
-How to set the configuration: 
-Input the names of your column headers in the fields above and click save. You
-must type them EXACTLY as they appear on the spreadsheet. This only needs to be
-done once unless you change the headers on your excel file.""")
+        self.display_markdown_file("files/settings_tut.md", self.announcement_box)
         self.announcement_box_font = font.Font(size=17, family="Helvetica")
         self.announcement_box.config(state="disabled", font=self.announcement_box_font, bg="#2b2d30")
         self.announcement_box.tag_configure("custom tag", foreground="white")
